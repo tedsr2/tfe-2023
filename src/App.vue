9b4f3f7b8a168d1781910154ee398673017ca549
Diego Tellier <96288744+tedsr2@users.noreply.github.com>
2023-03-30 12:00:47 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <div class="hello"><span class="child">Test nesting</span>Hello</div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped lang="scss">
+
+$bgColor: red;
+
+.hello {
+  background-color: $bgColor;
+  .child{
+    background-color: blue;
+  }
+}
+
+</style>
