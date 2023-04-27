c580166c70db079315521c7896d52e1bd6b5263c
Diego Tellier <96288744+tedsr2@users.noreply.github.com>
2023-04-27 23:07:15 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,47 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Intro from './components/Intro.vue';
+import Home from './components/Home.vue';
+import { ref, onMounted } from 'vue';
+import gsap from 'gsap';
+
+// const container = ref(null);
+// const content = ref(null);
+
+// onMounted(() => {
+//   gsap.from(container.value, {
+//     delay: 0.5,
+//     duration: 1,
+//     y: '+100',
+//     autoAlpha: 0,
+//     ease: 'black.out(1.7)',
+//   })
+// })
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <div class="hello"><span class="child">Test nesting</span>Hello</div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="main">
+    <!-- <div ref="container">
+      <div ref="content">
+        <div class="hello"><span class="child">Test nesting</span>Hello</div>
+        <div>Wesh</div>
+        <div>Wesh</div>
+        <div>Wesh</div>
+      </div>
+    </div> -->
+    <!-- <Intro /> -->
+    <Home />
+  </main>
 </template>
 
 <style scoped lang="scss">
+// $bgColor: red;
 
-$bgColor: red;
+// .hello {
+//   background-color: $bgColor;
 
-.hello {
-  background-color: $bgColor;
-  .child{
-    background-color: blue;
-  }
-}
-
+//   .child {
+//     background-color: blue;
+//   }
+// }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="home-container">
+        <img src='src/assets/sound.jpg' class="background" alt="" />
+        <div class="title-container">
+            <h1 class="title">soundtrack<span class="tracking">s</span></h1>
+            <h2 class="subtitle">the sound of movie<span class="tracking">s</span></h2>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.home-container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+
+    .background {
+        bottom: 0;
+        position: absolute;
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+    }
+
+    .title-container {
+        z-index: 1;
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        text-transform: uppercase;
+        padding-top: 60px;
+
+        .title {
+            font-size: 4.5rem;
+            font-weight: 400;
+            letter-spacing: 1em;
+            margin: 20px 0px;
+        }
+
+        .subtitle {
+            font-size: 1.3rem;
+            font-weight: 500;
+            letter-spacing: 0.6em;
+        }
+
+        .tracking {
+            letter-spacing: 0;
+        }
+
+    }
+}
+</style>
